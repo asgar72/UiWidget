fb9b6c39ef864f7757e40cbd46bb6d8da39585d5
Asgar Abbas <asgarabbas532@gmail.com>
2023-01-15 22:46:11 +0530

    ArrayAdaptor

---
+++ app/src/main/java/com/asgar/uiwidgets/AdaptorsDemoThree/MainActivityThree.java
@@ -0,0 +1,16 @@
+package com.asgar.uiwidgets.AdaptorsDemoThree;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.asgar.uiwidgets.R;
+
+public class MainActivityThree extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_three);
+    }
+}
